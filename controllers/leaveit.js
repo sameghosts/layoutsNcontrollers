ebbf919d0ffda5165871ce0f1e3d6c3a8ffb1536
sameghosts <seanfeiner.brown@gmail.com>
2021-01-10 23:13:59 -0800

    file structure for loveit and leaveit, leaveit controller

---
+++ controllers/leaveit.js
@@ -0,0 +1,13 @@
+const EXPRESS = require('express');
+const ROUTER = EXPRESS.Router();
+
+//movies route
+ROUTER.get('/movies', (req, res) => {
+  res.render('leaveit/movies', {movies: ['forrest gump', 'Crash(not the Sebald / Cronenburg one)', 'Pretty much any comedy but especially anything starring Will Ferrel', 'Goodfellas', 'The Departed', 'Any Marvel Movie']});
+});
+//products route
+ROUTER.get('/products', (req, res) => {
+  res.render('/leaveit/products', {products: ['single ply toilet paper', 'shitty paper towels', 'nonstick teflon cookwear', 'high socks', 'overhyped prestige television', 'in ear headphones', 'kerug coffee makers']});
+});
+
+module.exports = ROUTER;
--- index.js
+++ index.js
@@ -15,20 +15,20 @@ APP.get('/', (req, res) => {
   res.render('home');
 });
 //animals route
-APP.get('/loveit/animals', (req, res) => {
-  res.render('animals', {animals: ['sand crab', 'corny joke dog']});
-});
+// APP.get('/loveit/animals', (req, res) => {
+//   res.render('animals', {animals: ['sand crab', 'corny joke dog']});
+// });
 //foods route
-APP.get('/loveit/foods', (req, res) => {
-  res.render('foods', {foods: ['kimchee', 'salmon', 'crawdad boil', 'alomonds', 'peanut butter', 'feta cheese and olives', 'kale', 'brussel sprouts', 'sherbert', 'chili']});
-});
-//movies route
-APP.get('/leaveit/movies', (req, res) => {
-  res.render('movies', {movies: ['forrest gump', 'Crash(not the Sebald / Cronenburg one)', 'Pretty much any comedy but especially anything starring Will Ferrel', 'Goodfellas', 'The Departed', 'Any Marvel Movie']});
-});
-//products route
-APP.get('/leaveit/products', (req, res) => {
-  res.render('products', {products: ['single ply toilet paper', 'shitty paper towels', 'nonstick teflon cookwear', 'high socks', 'overhyped prestige television', 'in ear headphones', 'kerug coffee makers']});
-});
+// APP.get('/loveit/foods', (req, res) => {
+//   res.render('foods', {foods: ['kimchee', 'salmon', 'crawdad boil', 'alomonds', 'peanut butter', 'feta cheese and olives', 'kale', 'brussel sprouts', 'sherbert', 'chili']});
+// });
+// //movies route
+// APP.get('/leaveit/movies', (req, res) => {
+//   res.render('movies', {movies: ['forrest gump', 'Crash(not the Sebald / Cronenburg one)', 'Pretty much any comedy but especially anything starring Will Ferrel', 'Goodfellas', 'The Departed', 'Any Marvel Movie']});
+// });
+// //products route
+// APP.get('/leaveit/products', (req, res) => {
+//   res.render('products', {products: ['single ply toilet paper', 'shitty paper towels', 'nonstick teflon cookwear', 'high socks', 'overhyped prestige television', 'in ear headphones', 'kerug coffee makers']});
+// });
 
 APP.listen(8710);
